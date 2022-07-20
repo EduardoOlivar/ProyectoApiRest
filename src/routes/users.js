d9ed7ec355dec5312a1e876bb267c1cfe8a8d62b
Eduardo Olivar <eduardo.olivar.aros@gmail.com>
2022-07-19 23:28:20 -0400

    Se agrego mensajes de error, queda por solucionar el mensaje de error en la parte de delete

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,17 +12,14 @@ router.get('/',(req,res)=>{
 //obtiene usuario por su id
 router.get('/:id',(req,res)=>{
     const {id}  = req.params;
-    under.find(usuarios,(user,i)=>{
-        if(user.id == id){
-            res.json(usuarios[i]);
-        }
-    });
-    //este for sirve igual para mostrar usuario por id
-    // for(let i =0; i < usuarios.length; i++){
-    //     if(usuarios[i].id == id){
-    //         res.json(usuarios[i]);
-    //     }
-    // }
+
+    const user =usuarios.find((user)=> user.id == id);
+    if(user){
+        res.json(user);
+    }else{
+        res.status(400).json({error:'No se encontro el id'});
+    }
+    
 });
 
 //agrega usuarios
@@ -45,18 +42,12 @@ router.delete('/:id', (req,res) => {
     const {id}  = req.params;
     for(let i =0; i < usuarios.length; i++){
         if(usuarios[i].id == id){
-            usuarios.splice(i,1);
+        usuarios.splice(i,1);
+        res.send(usuarios);
         }
     }
 
-// queda comentado porque tira un error con la funcion splice, sin embargo funciona al segundo intento
-//    under.each(usuarios, (user,i) => {
-//         const {id}  = req.params;
-//         if(user.id == id){
-//             usuarios.splice(i,1);
-//         }
-//     });
-    res.send(usuarios);
+    // res.status(400).json({error:'No se encontro el id'});
 });
 
 
@@ -81,15 +72,19 @@ router.patch('/:id',(req,res)=>{
     const {id}  = req.params;
     const {nombre,apellidos}= req.body;
     const user = usuarios.find((user)=> user.id == id);
-    if(nombre)
-    {
-        user.nombre = nombre;
+    if(user){
+        if(nombre)
+        {
+            user.nombre = nombre;
+        }
+        if(apellidos)
+        {
+            user.apellidos = apellidos;
+        }
+        res.json(user);
+    }else{
+        res.status(400).json({error: "No se encontro el id deseado"});
     }
-    if(apellidos)
-    {
-        user.apellidos = apellidos;
-    }
-    res.json(usuarios);
 
 });
 
